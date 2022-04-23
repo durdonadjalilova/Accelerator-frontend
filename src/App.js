009968f0e623896ce4aba885d8a631bf4ce6c0f1
Durdona Djalilova <ddjalilova3@gmail.com>
2022-04-23 17:36:46 -0400

    learn arabic frontend start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ArabLetters from "./components/arabLetters/ArabLetters";
+import SingleLetter from "./components/letterDetail/SingleLetter";
 import "./App.css";
 
 function App() {
-  return <div className="App">Awesome new App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ArabLetters />} />
+          <Route path="/letter/:id" element={<SingleLetter />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
